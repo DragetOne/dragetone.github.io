66741837d67db60f7b05c254e0a50438e439f9b6
DragetOne <draget.one@gmail.com>
2023-11-23 14:40:13 -0600

    Small styles changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// JavaScript
 let currentIndex = 0;
 let timer;
 
@@ -25,7 +24,7 @@ function changeSlide(direction) {
 
 function resetTimer() {
     clearInterval(timer);
-    timer = setInterval(() => changeSlide(1), 7000); // Change slide every 7 seconds (adjust as needed)
+    timer = setInterval(() => changeSlide(1), 7000); // Change slide every 7 seconds
 }
 
 // Initial timer setup
