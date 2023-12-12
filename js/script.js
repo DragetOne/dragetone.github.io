b0ae442362b3bea2e0ada5309bf6fb38c6192f2e
DragetOne <draget.one@gmail.com>
2023-12-12 15:53:08 -0600

    Adding Let's Draw app to the carousel

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ function changeSlide(direction) {
 
 function resetTimer() {
     clearInterval(timer);
-    timer = setInterval(() => changeSlide(1), 5000); // Change slide every 5 seconds
+    timer = setInterval(() => changeSlide(1), 3000); // Change slide every 3 seconds
 }
 
 // Initial timer setup
